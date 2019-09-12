1b91ba763fa88e395cae0299d638ed32441082f6
Ubuntu <ubuntu@ip-172-31-29-95.ec2.internal>
2019-09-12 18:51:09 +0000

    initial message

---
+++ hello_world_five.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# created by: Izza Khalid
+# created on: September 2019
+# This is the "Hello, world!" program, but with nice style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
